556690f886bfdff732e36cadbe40424bbe0f576e
Philip Laine <philip.laine@gmail.com>
2019-10-23 21:18:00 +0200

    Update code to take message as env var

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"strings"
+	"os"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Lainecloud is up and running"
-
-	w.Write([]byte(message))
-}
-
 func main() {
-	http.HandleFunc("/", index)
+	message := os.Getenv("MESSAGE")
+	if message == "" {
+		message = "Hello World"
+	}
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(message))
+	})
+
+	log.Println("Serving web page")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
